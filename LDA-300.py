38697487218033e20d69674c5aaa13d5703c4bf7
Jeafi <651699394@qq.com>
2018-03-17 20:38:24 +0800

    runable
    可以进行遍历运行
    tdidf效果好于LDA模型

--- LDA-300.py
+++ LDA-300.py
@@ -1,13 +1,30 @@
-from gensim import corpora,models,similarities,utils
+from gensim import corpora, models, similarities, utils
 import os
+# 先对文件重新编号
 fa = os.listdir(r'alltext')
 f = r'alltext'
+fr = r'result'
+# f1是案件名称
 for f1 in fa:#遍历整个一级文件夹
     train_set = []
-    tmp_pathall = os.path.join(f, fa)
+    tmp_pathall = os.path.join(f, f1)
     fs = os.listdir(tmp_pathall)
-    for f1 in fs:#遍历单个案由的所有文件
-        tmp_path = os.path.join(tmp_pathall,fs)
+    i=0
+    for f2 in fs:#遍历单个案由的所有文件
+        j = '00'+str(i)+'.txt'
+        tmp_path = os.path.join(tmp_pathall,f2)
+        os.rename(tmp_path, os.path.join(tmp_pathall,j))
+        i = i+1
+# 编号完毕
+fa = os.listdir(r'alltext')
+f = r'alltext'
+fr = r'result'
+for f1 in fa:  # 遍历整个一级文件夹
+    train_set = []
+    tmp_pathall = os.path.join(f, f1)
+    fs = os.listdir(tmp_pathall)
+    for f2 in fs:  # 遍历单个案由的所有文件
+        tmp_path = os.path.join(tmp_pathall, f2)
         ff = open(tmp_path, encoding='utf-8')
         lines = ff.readlines()
         text = []
@@ -15,36 +32,54 @@ for f1 in fa:#遍历整个一级文件夹
         for line in lines:
             if len(line) != 1:
                 # 抽掉引用法条
-                if line.split('##')[0] != '引用法条' and line.split('##')[0] != '特征':
+                if line.split('##')[0] != '引用法条' and line.split('##')[0] != '特征' and line.split('##')[0] != '案由':
                     for word in line.split('##')[1].split('$'):
                         text.append(word.replace('\n', '').replace('\t', ''))
                 if line.split('##')[0] == '特征':
                     for word in line.split('##')[1].split('\t'):
                         text.append(word.replace('\n', '').replace('\t', ''))
-    train_set.append(text)
-    output = fs #输出文件目录
-    # 生成字典
+                if line.split('##')[0] == '案由':
+                    for word in line.split('##')[1].split('\t'):
+                        text.append(word.replace('\n', '').replace('\t', ''))
+        train_set.append(text)
+    output = os.path.join(fr, f1)  # 输出文件目录
+    if not os.path.exists(output):
+        os.makedirs(output)
+        # 生成字典
     dictionary = corpora.Dictionary(train_set)
     # 去除极低频的杂质词
+
     dictionary.filter_extremes(no_below=1, no_above=1, keep_n=None)
     # 将词典保存下来，方便后续使用
-    dictionary.save(output + "all.dic")
+
+    dictionary.save(os.path.join(output, "all.dic"))
+
     corpus = [dictionary.doc2bow(text) for text in train_set]
     # 使用数字语料生成TFIDF模型
+
     tfidfModel = models.TfidfModel(corpus)
     # 存储tfidfModel
-    tfidfModel.save(output + "allTFIDF.mdl")
+
+    tfidfModel.save(os.path.join(output, "allTFIDF.mdl"))
     # 把全部语料向量化成TFIDF模式，这个tfidfModel可以传入二维数组
+
     tfidfVectors = tfidfModel[corpus]
     # 建立索引并保存
+
     indexTfidf = similarities.MatrixSimilarity(tfidfVectors)
-    indexTfidf.save(output + "allTFIDF.idx")
+
+    indexTfidf.save(os.path.join(output, "allTFIDF.idx"))
     # 通过TFIDF向量生成LDA模型，id2word表示编号的对应词典，num_topics表示主题数，我们这里设定的50，主题太多时间受不了。
+
     lda = models.LdaModel(tfidfVectors, id2word=dictionary, num_topics=100)
     # 把模型保存下来
-    lda.save(output + "allLDA50Topic.mdl")
+
+    lda.save(os.path.join(output, "allLDA50Topic.mdl"))
     # 把所有TFIDF向量变成LDA的向量
+
     corpus_lda = lda[tfidfVectors]
     # 建立索引，把LDA数据保存下来
+
     indexLDA = similarities.MatrixSimilarity(corpus_lda)
-    indexLDA.save(output + "allLDA50Topic.idx")
+
+    indexLDA.save(os.path.join(output, "allLDA50Topic.idx"))
--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 from gensim import corpora,models,similarities,utils
 import os
-f = open('测试文档路径')
+f = open(r'alltext\绑架罪\000.txt',encoding='utf-8')
 lines = f.readlines()
 text = []
 anyou = ''
 for line in lines:#将文件中的词装入列表
      if len(line) != 1:
-         if line.split('##')[0] != '引用法条' and line.split('##')[0] != '特征':
-             for word in line.split('##')[1].split('$'):
-                 text.append(word.replace('\n', '').replace('\t', ''))
-         if line.split('##')[0] == '特征':
-             for word in line.split('##')[1].split('\t'):
-                text.append(word.replace('\n', '').replace('\t', ''))
-         if line.split('##')[0] == '案由':
-             anyou = line.split('##')[1]
-             anyou = anyou.replace('\n', '').replace('\t', '')
+            # 抽掉引用法条
+            if line.split('##')[0] != '引用法条' and line.split('##')[0] != '特征' and line.split('##')[0] != '案由':
+                for word in line.split('##')[1].split('$'):
+                    text.append(word.replace('\n', '').replace('\t', ''))
+            if line.split('##')[0] == '特征':
+                for word in line.split('##')[1].split('\t'):
+                    text.append(word.replace('\n', '').replace('\t', ''))
+            if line.split('##')[0] == '案由':
+                for word in line.split('##')[1].split('\t'):
+                    text.append(word.replace('\n', '').replace('\t', ''))
 #到该案由路径下载入获取已经训练好的模型
-output = os.path.join ('alltext',anyou)
+fr = r'result'
+output = os.path.join (fr,'绑架罪')
 # 载入字典
 dictionary = corpora.Dictionary.load(os.path.join(output,"all.dic"))
 # 载入TFIDF模型和索引
@@ -35,7 +37,6 @@ ldavec = ldaModel[tfidfvect]
 simstfidf = indexTfidf[tfidfvect]
 #LDA相似性，相似性列表记录与每份模型中的文书的相似度
 simlda = indexLDA[ldavec]
-
 # 获取TDIDF模型中与该文档前十相近的文书号
 # 记录序号
 num = []
@@ -45,11 +46,11 @@ for i in range(0,10):
     # 相似度
     m = 0
     # 文书序号
-    mark = 1
+    mark = 0
     for likely in simstfidf:
-        if m < likely and m not in gets:
-            m = likely
-            marknow = mark
+        if m < likely and likely not in gets:
+                m = likely
+                marknow = mark
         mark = mark+1
     gets.append(m)
     num.append(marknow)
@@ -64,9 +65,9 @@ for i in range(0,10):
     # 相似度
     m = 0
     # 文书序号
-    mark = 1
+    mark = 0
     for likely in simlda:
-        if m < likely and m not in gets:
+        if m < likely and likely not in getslda:
             m = likely
             marknow = mark
         mark = mark+1
